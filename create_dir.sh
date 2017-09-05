40e2752a92f5ada6c97142bece60c97ed4948fa6
Sirish Reddy <sredd1@sapient.com>
2017-09-05 12:13:39 +0100

    added folder structure

---
+++ create_dir.sh
@@ -0,0 +1,92 @@
+mkdir Colleague
+mkdir -p Colleague/ColleagueApplication
+mkdir -p Colleague/ColleagueApplication/.apt_generated
+mkdir -p Colleague/ColleagueApplication/.externalToolBuilders
+mkdir -p Colleague/ColleagueApplication/.metadata
+mkdir -p Colleague/ColleagueApplication/.settings/
+mkdir -p Colleague/ColleagueApplication/build
+mkdir -p Colleague/ColleagueApplication/src
+mkdir -p Colleague/ColleagueApplication/src/main
+mkdir -p Colleague/ColleagueApplication/src/main/configTBT
+mkdir -p Colleague/ColleagueApplication/src/main/java
+mkdir -p Colleague/ColleagueApplication/src/main/sonar
+mkdir -p Colleague/ColleagueApplication/src/main/java/com
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/activateusername
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/adddeletenonlbgaccount
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/assignunassignrole
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/auditreports
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/common
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/createcustomer
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/createcustomer/bean
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/createcustomer/constants
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/createcustomer/helper
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/createcustomer/mapper
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/createcustomer/pfo
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/colleague/createcustomer/po
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/createssa
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/createuser
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/customermessage
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/enablelogin
+mkdir -p Colleague/ColleagueApplication/src/main/java/com/lbg/tbt/exception
+
+mkdir Customer
+mkdir -p Customer/ActivateLoLiCard
+mkdir -p Customer/ChangePassword
+mkdir -p Customer/CreateAndMaintianUser
+mkdir -p Customer/.apt_generated
+mkdir -p Customer/.metadata
+mkdir -p Customer/.settings/
+mkdir -p Customer/coverage-report
+mkdir -p Customer/doc
+mkdir -p Customer/src
+mkdir -p Customer/src/main
+mkdir -p Customer/src/java
+mkdir -p Customer/src/java/com
+mkdir -p Customer/src/java/com/lbg/tbt/
+mkdir -p Customer/src/java/com/lbg/tbt/activateusername
+mkdir -p Customer/src/java/com/lbg/tbt/assigndevice
+mkdir -p Customer/src/java/com/lbg/tbt/assignunassignrole
+mkdir -p Customer/src/java/com/lbg/tbt/changeowntelephonypin
+mkdir -p Customer/src/java/com/lbg/tbt/createandmaintainuser
+mkdir -p Customer/src/java/com/lbg/tbt/createuser
+mkdir -p Customer/src/java/com/lbg/tbt/createuser/bean
+mkdir -p Customer/src/java/com/lbg/tbt/createuser/helper
+mkdir -p Customer/src/java/com/lbg/tbt/createuser/mapper
+mkdir -p Customer/src/java/com/lbg/tbt/createuser/nl
+mkdir -p Customer/src/java/com/lbg/tbt/createuser/pfo/
+mkdir -p Customer/src/java/com/lbg/tbt/createuser/po
+mkdir -p Customer/src/java/com/lbg/tbt/createuser/portlet
+mkdir -p Customer/src/java/com/lbg/tbt/exception
+mkdir -p Customer/src/java/com/lbg/tbt/maintainuser
+mkdir -p Customer/src/java/com/lbg/tbt/maintainuserdetails
+mkdir -p Customer/src/java/com/lbg/tbt/managecredentials
+mkdir -p Customer/src/java/com/lbg/tbt/managelandingpages
+mkdir -p Customer/src/java/com/lbg/tbt/managereplacementdevice
+mkdir -p Customer/src/java/com/lbg/tbt/navigation
+mkdir -p Customer/src/java/com/lbg/tbt/orderdevices
+
+mkdir framework
+mkdir -p framework/bin
+mkdir -p framework/build
+mkdir -p framework/src
+mkdir -p framework/src/main/java/com/lbg/tbt/
+mkdir -p framework/src/main/java/com/lbg/tbt/businessoperations
+mkdir -p framework/src/main/java/com/lbg/tbt/batch
+mkdir -p framework/src/main/java/com/lbg/tbt/dao
+mkdir -p framework/src/main/java/com/lbg/tbt/dao/impl
+mkdir -p framework/src/main/java/com/lbg/tbt/dao/mapper
+mkdir -p framework/src/main/java/com/lbg/tbt/delegate
+mkdir -p framework/src/main/java/com/lbg/tbt/delegate/impl
+
+
+
+
+
+
+
+
+
+
